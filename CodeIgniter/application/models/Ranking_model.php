b2f34e08dd33df76e9ac515750bb1df13e9b11dc
LOUDIYI SAMMY <sloudiy2@pctest14-04>
2017-10-10 17:10:57 +0200

    Add Ranking file (draft)

---
+++ CodeIgniter/application/models/Ranking_model.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sloudiy2
+ * Date: 10/10/17
+ * Time: 16:59
+ */
+
+class Ranking_model extends CI_Model{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function weekly(){
+
+    }
+    public function monthly(){
+
+    }
+    public function yearly(){
+
+    }
+    public function generalWeekly(){
+
+    }
+    public function generalMonthly(){
+
+    }
+    public function generalYearly(){
+
+    }
+
+
+
+}
