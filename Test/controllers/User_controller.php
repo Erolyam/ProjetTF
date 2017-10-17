40a3734517bceed5e44d5727a928eb5dd264d1f3
Diego Romero <dromeror@unal.edu.co>
2017-10-17 20:42:27 +0200

    Test unitaires pour le model et le controller.

--- Test/controllers/User_controller.php
+++ Test/controllers/User_controller.php
@@ -9,13 +9,13 @@ namespace controllers\tests\units;
 use \atoum;
 class User_controller extends atoum
 {
-//    public function test_username_exists()
-//    {
-//        $this
-//            ->if($c = new \controllers\User_controller(false))
-//            ->then
-//            ->boolean($c->is_available_email("ewuqeuwqyiweq@gmail.com"))
-//            ->isIdenticalTo(true);
-//        return 0;
-//    }
+    public function test_username_exists()
+    {
+        $this
+            ->if($c = new \controllers\User_controller(false))
+            ->then
+            ->boolean($c->is_available_email("ewuqeuwqyiweq@gmail.com"))
+            ->isIdenticalTo(true);
+        return 0;
+    }
 }
